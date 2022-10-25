0357a6c057218d24e0549fe1887f3cf12ed0706d
John Harold Obrero <obreroharoldjohn@gmail.com>
2022-10-25 16:01:53 +0800

    Setup of jwt 2.0

---
+++ config/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+    ],
+
+];
